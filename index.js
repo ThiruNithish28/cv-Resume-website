cecfc9e311338356e114313d0312c4b8e27fa877
ThiruNithish28 <valavannithish.007@gmail.com>
2023-10-04 20:38:51 +0530

    update the files

--- index.js
+++ index.js
@@ -40,3 +40,20 @@ $(".btn_nav").click(function () {
     }, 1500);
   });
   
+
+//   hanberge menu 
+
+const navList = document.querySelector(".nav-lists");
+const navListLinks = document.querySelector(".nav-lists-links")
+const navToggle = document.querySelector(".menu-toggle");
+
+navToggle.addEventListener("click", () =>{
+    const visibility = navList.getAttribute("data-visible");
+    if(visibility === "false"){
+        navList.setAttribute('data-visible',true);
+        navToggle.setAttribute('aria-expanded',true);
+    }else if(visibility === "true"){
+        navList.setAttribute('data-visible',false);
+        navToggle.setAttribute('aria-expanded',false);
+    }
+})
